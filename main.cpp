296702891b3b8f2d1a08fa83d0dd296cd6b9dc02
Juanjo <juanjod@gmail.com>
2016-02-01 12:27:07 +0100

    added code to create context and command queue

--- main.cpp
+++ main.cpp
@@ -39,47 +39,53 @@ int main() {
 		exit(status);
 	}
 
-	//--------------------------------------------------------//
-	//---------printing_platforms_information-----------------//
-	//--------------------------------------------------------//
-
-	for (unsigned int i = 0; i < numPlatforms; i++) {
-
-		status = clGetDeviceIDs(platforms[i], CL_DEVICE_TYPE_ALL, 0, NULL, &numDevices);
-		if (status != 0) {
-			printf("Error %d while retrieving number of devices for platform %d\n", status, i);
-			exit(-1);
-		}
-
-		// allocate space for devices
-		devices = (cl_device_id *)malloc(sizeof(cl_device_id)*numDevices);
-		if (devices == NULL) {
-			printf("Error allocating memory to store devices id for platform %d\n", i);
-			exit(-1);
-		}
-
-		//we obtain them althoug so far we do nothing with them
-		status = clGetDeviceIDs(platforms[i], CL_DEVICE_TYPE_ALL, numDevices, devices, 0);
-
-		
-		char buffer[10240];
-		printf("  Information about platform -- %d --\n", i);
-		printf("The platform has %d devices \n",numDevices);
-		clGetPlatformInfo(platforms[i], CL_PLATFORM_PROFILE, 10240, buffer, NULL);
-		printf("  PROFILE = %s\n", buffer);
-		clGetPlatformInfo(platforms[i], CL_PLATFORM_VERSION, 10240, buffer, NULL);
-		printf("  VERSION = %s\n", buffer);
-		clGetPlatformInfo(platforms[i], CL_PLATFORM_NAME, 10240, buffer, NULL);
-		printf("  NAME = %s\n", buffer);
-		clGetPlatformInfo(platforms[i], CL_PLATFORM_VENDOR, 10240, buffer, NULL);
-		printf("  VENDOR = %s\n", buffer);
-		clGetPlatformInfo(platforms[i], CL_PLATFORM_EXTENSIONS, 10240, buffer, NULL);
-		printf("  EXTENSIONS = %s\n", buffer);
-		free(devices);
+	status = clGetDeviceIDs(platforms[numPlatforms-1], CL_DEVICE_TYPE_ALL, 0, NULL, &numDevices);
+	if (status != 0) {
+		printf("Error %d while retrieving number of devices for platform %d\n", status, numPlatforms-1);
+		exit(-1);
 	}
 
+	// allocate space for devices
+	devices = (cl_device_id *)malloc(sizeof(cl_device_id)*numDevices);
+	if (devices == NULL) {
+		printf("Error allocating memory to store devices id for platform %d\n", numPlatforms-1);
+		exit(-1);
+	}
 
-	//free host resources	
+	//we obtain them althoug so far we do nothing with them
+	status = clGetDeviceIDs(platforms[numPlatforms-1], CL_DEVICE_TYPE_ALL, numDevices, devices, 0);
+	if (status != 0) {
+		printf("Error %d when obtaining devices on platform %d\n", status, numPlatforms - 1);
+		exit(status);
+	}
+		
+	//creating a context 
+	//-------------creating a context-------------------------//
+	cl_context context = NULL;
+	context = clCreateContext(NULL, numDevices, devices, NULL, NULL, &status);
+	if (status != 0) {
+		printf("Error %d when creating the context in platform %d\n", status, numPlatforms - 1);
+		exit(status);
+	}
+
+	// creating a command queue
+	cl_int selectedDevice = 0;
+	cl_command_queue cmdQueue;
+
+	cl_queue_properties qprop[] = { CL_QUEUE_PROPERTIES,(cl_command_queue_properties)CL_QUEUE_OUT_OF_ORDER_EXEC_MODE_ENABLE |
+									CL_QUEUE_PROFILING_ENABLE, 0 };
+	
+
+	cmdQueue = clCreateCommandQueueWithProperties(context, devices[selectedDevice],qprop, &status);
+	if (status != 0) {
+		printf("Error %d when creating the queue for the device %d on platform %d\n", status, selectedDevice, numPlatforms - 1);
+		exit(status);
+	}
+	
+	//free host resources
+	clReleaseContext(context);
+	clReleaseCommandQueue(cmdQueue);
+	free(devices);
 	free(platforms);
 	return 0;
 }
