2fd30fe9f2ac5e835e0434db96aaad55050f1403
Juanjo <juanjod@gmail.com>
2016-02-05 14:07:02 +0100

    Example queuing from device

--- Kernels.cl
+++ Kernels.cl
@@ -1,4 +1,18 @@
-__kernel void foo(global float *p) {
- int idx = get_global_id(0);
- printf("Hola %f\n",*(p+idx));
- }
+__kernel void vecadd(__global int *A, __global int *B, __global int *C) {
+	printf("child\n");
+	int idx = get_global_id(0);
+	C[idx] = A[idx] + B[idx];
+}
+
+
+__kernel void parent_vecadd(__global int *A, __global int* B, __global int* C) {
+	ndrange_t child_ndrange = ndrange_1D(get_global_size(0));
+	printf("parent\n");
+	//only enqueue one child kernel
+	if (get_global_id(0) == 0) {
+		enqueue_kernel(get_default_queue(),
+			CLK_ENQUEUE_FLAGS_NO_WAIT,
+			child_ndrange,
+			^{ vecadd(A, B, C); });
+	}
+}
--- main.cpp
+++ main.cpp
@@ -1,180 +1,214 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <CL/cl.h>
+#include <CL/opencl.h>
 
-#define FILE_MAX_LENGTH 1000000
-char *createProgramFromFile(const char*fileName, size_t *sourceSize) {
-	//const char fileName[] = "matrixmul2.cl";
+#define MAX_SOURCE_SIZE 1000000
+
+int main() {
+	int *A = NULL; // input array
+	int *B = NULL; // input array
+	int *C = NULL; // input array
+
+	//Elements in each array
+	const int elements = 12;
+
+	//size of the data
+	size_t datasize = sizeof(int)*elements;
+
+	// Allocate space for input/output data
+	A = (int *)malloc(datasize);
+	B = (int *)malloc(datasize);
+	C = (int *)malloc(datasize);
+
+	// Initialize the input data
+	for (int i = 0; i < elements; i++)  {
+		A[i] = i;
+		B[i] = i;
+	}
+
+
+	//it is a good practice to check the status of each API call
+	cl_int status;
+
+	cl_uint numPlatforms = 0;
+	cl_platform_id *platforms = NULL;
+
+
+	//---------------------------------------------------------//
+	//--------------retrieving platforms-----------------------//	
+	// the number of platforms is retrieved by using a first call
+	// to clGetPlatformsIDs() with NULL argument as second argument
+	status = clGetPlatformIDs(0, NULL, &numPlatforms);
+	platforms = (cl_platform_id *)malloc(
+		numPlatforms * sizeof(cl_platform_id));
+
+	printf("Number of platforms: %d\n", numPlatforms);
+	// the second call, get the platforms
+	status = clGetPlatformIDs(numPlatforms, platforms, NULL);
+
+	//--------------------------------------------------------//
+	//-------------retrieving devices ------------------------//
+	cl_uint numDevices = 0;
+	cl_device_id *devices = NULL;
+
+	// as before, the devices are retrieved in two steps
+	status = clGetDeviceIDs(platforms[2], CL_DEVICE_TYPE_ALL, 0, NULL, &numDevices);
+	printf("Number of devices: %d\n", numPlatforms);
+	// allocating space for the devices
+	devices = (cl_device_id *)malloc(numDevices * sizeof(cl_device_id));
+
+	// the second call get the devices
+	status = clGetDeviceIDs(platforms[2], CL_DEVICE_TYPE_ALL, numDevices, devices, NULL);
+
+	//--------------------------------------------------------//
+	//-------------creating a context-------------------------//
+	cl_context context = NULL;
+	context = clCreateContext(NULL, numDevices, devices, NULL, NULL, &status);
+
+	//--------------------------------------------------------//
+	//-------------creating a comand queue -------------------//
+	cl_command_queue cmdQueue;
+	cmdQueue = clCreateCommandQueue(context, devices[0], 0, &status);
+	cl_command_queue_properties properties = CL_QUEUE_ON_DEVICE | CL_QUEUE_OUT_OF_ORDER_EXEC_MODE_ENABLE | CL_QUEUE_ON_DEVICE_DEFAULT;
+
+
+	cl_queue_properties qprop[] = { CL_QUEUE_SIZE, 10000000, CL_QUEUE_PROPERTIES,
+		(cl_command_queue_properties)CL_QUEUE_OUT_OF_ORDER_EXEC_MODE_ENABLE | CL_QUEUE_ON_DEVICE | CL_QUEUE_ON_DEVICE_DEFAULT, 0 };
+
+	//cl_command_queue my_device_q = clCreateCommandQueueWithProperties(CLU_CONTEXT, cluGetDevice(CL_DEVICE_TYPE_GPU), qprop, &status);
+	cl_command_queue my_device;
+	my_device = clCreateCommandQueueWithProperties(context, devices[0], qprop, &status);
+	if (status != 0) {
+		printf("Error al crear la cola: %d\n", status);
+		while (true) {
+			;
+		}
+		exit(status);
+	}
+
+
+	//--------------------------------------------------------//
+	//-------------creating device buffers -------------------//
+	cl_mem bufferA; // Input Array on the device
+	cl_mem bufferB; // Input Array on the device
+	cl_mem bufferC; // Output Array on the device
+
+	bufferA = clCreateBuffer(context, CL_MEM_READ_ONLY, datasize, NULL, &status);
+	bufferB = clCreateBuffer(context, CL_MEM_READ_ONLY, datasize, NULL, &status);
+	bufferC = clCreateBuffer(context, CL_MEM_WRITE_ONLY, datasize, NULL, &status);
+
+	// ------------------------------------------------------//
+	//-------------writing the data on the device------------//	
+	// write host data to device buffer (similar to CUDA_memcpy)
+	status = clEnqueueWriteBuffer(cmdQueue, bufferA, CL_FALSE, 0, datasize, A, 0, NULL, NULL);
+	status = clEnqueueWriteBuffer(cmdQueue, bufferB, CL_FALSE, 0, datasize, B, 0, NULL, NULL);
+
+	//-------------------------------------------------------//
+	//-------------CREATE AND COMPILE THE PROGRAM------------//
+	// 6. Load and build OpenCL kernel
+	const char fileName[] = "kernels.cl";
+	size_t sourceSize;
 	char *source_str;
 	FILE* fp = fopen(fileName, "rb");
 	if (!fp) {
 		printf("Error while loading the source code %s\n", fp);
-		exit(-1);
+		exit(1);
 	}
-	source_str = (char *)malloc(sizeof(char)*FILE_MAX_LENGTH);
-	*sourceSize = fread(source_str, 1, FILE_MAX_LENGTH, fp);
+	source_str = (char *)malloc(sizeof(char)*MAX_SOURCE_SIZE);
+
+	sourceSize = fread(source_str, 1, MAX_SOURCE_SIZE, fp);
+
 	fclose(fp);
-	return source_str;
-}
 
-int main() {
-
-	cl_int status;
-	cl_uint numPlatforms = 0;
-	cl_uint numDevices = 0;
-	cl_platform_id *platforms = NULL;
-	cl_device_id   *devices = NULL;
-
-
-	// the number of platforms is retrieved by using a first call
-	// to clGetPlatformsIDs() with NULL argument as second argument
-	status = clGetPlatformIDs(0, NULL, &numPlatforms);
-	if (status != 0) {
-		printf("Status value %d\n",status);
-		exit(status);
-	}
-
-	printf("Number of platforms: %d\n", numPlatforms);
-
-
-	// allocating memory for the platforms
-	platforms = (cl_platform_id *)malloc(numPlatforms * sizeof(cl_platform_id));
-
-	if (platforms == NULL) {
-		printf("Not enough memory\n");
-		exit(-1);
-	}
-
-	// the second call, get the platforms
-	status = clGetPlatformIDs(numPlatforms, platforms, NULL);
-	if (status != 0) {
-		printf("Status value %d\n", status);
-		exit(status);
-	}
-
-	status = clGetDeviceIDs(platforms[numPlatforms-1], CL_DEVICE_TYPE_ALL, 0, NULL, &numDevices);
-	if (status != 0) {
-		printf("Error %d while retrieving number of devices for platform %d\n", status, numPlatforms-1);
-		exit(-1);
-	}
-
-	// allocate space for devices
-	devices = (cl_device_id *)malloc(sizeof(cl_device_id)*numDevices);
-	if (devices == NULL) {
-		printf("Error allocating memory to store devices id for platform %d\n", numPlatforms-1);
-		exit(-1);
-	}
-
-	//we obtain them althoug so far we do nothing with them
-	status = clGetDeviceIDs(platforms[numPlatforms-1], CL_DEVICE_TYPE_ALL, numDevices, devices, 0);
-	if (status != 0) {
-		printf("Error %d when obtaining devices on platform %d\n", status, numPlatforms - 1);
-		exit(status);
-	}
-		
-	//creating a context 
-	//-------------creating a context-------------------------//
-	cl_context context = NULL;
-	context = clCreateContext(NULL, numDevices, devices, NULL, NULL, &status);
-	if (status != 0) {
-		printf("Error %d when creating the context in platform %d\n", status, numPlatforms - 1);
-		exit(status);
-	}
-
-	// creating a command queue
-	cl_int selectedDevice = 0;
-	cl_command_queue cmdQueue;
-
-	cl_queue_properties qprop[] = { CL_QUEUE_PROPERTIES,(cl_command_queue_properties)CL_QUEUE_OUT_OF_ORDER_EXEC_MODE_ENABLE |
-									CL_QUEUE_PROFILING_ENABLE, 0 };
-	
-
-	cmdQueue = clCreateCommandQueueWithProperties(context, devices[selectedDevice],qprop, &status);
-	if (status != 0) {
-		printf("Error %d when creating the queue for the device %d on platform %d\n", status, selectedDevice, numPlatforms - 1);
-		exit(status);
-	}
-
-	// creating the program
-	size_t sourceSize;
-	char * programStr = createProgramFromFile("Kernels.cl",&sourceSize);
 
 	cl_program clProgram;
-	clProgram = clCreateProgramWithSource(context, 1, (const char **)&programStr, (const size_t*)&sourceSize, &status);
-	if (status != 0) {
-		printf("Error when creating the program from the sources %d\n", status);
-		exit(status);
+	cl_int errcode;
+	const char * options = "-cl-std=CL2.0 -D CL_VERSION_2_0";
+	clProgram = clCreateProgramWithSource(context, 1, (const char **)&source_str, (const size_t*)&sourceSize, &errcode);
+	if (errcode != 0) {
+		printf("Error: %d\n", errcode);
+		exit(errcode);
 	}
-	status = clBuildProgram(clProgram, 0, NULL, NULL, NULL, NULL);
-	if (status != 0) {
-		printf("Error when compiling the opencl code: %d\n", status);
+	errcode = clBuildProgram(clProgram, 0, NULL, options, NULL, NULL);
+	if (errcode != 0) {
+		printf("Error 2: %d\n", errcode);
+
 		// Allocate memory for the log
 		int log_size = 100000;
 		char *log = (char *)malloc(log_size);
 
 		// Get the log
-		clGetProgramBuildInfo(clProgram, devices[selectedDevice], CL_PROGRAM_BUILD_LOG, log_size, log, NULL);
-		printf("%s\n", log);
-		exit(status);
+		clGetProgramBuildInfo(clProgram, devices[0], CL_PROGRAM_BUILD_LOG, log_size, log, NULL);
+		printf("Error: %d\n", errcode);
+		// Print the log
+		//printf("%s\n", log);
+		while (true) {
+			;
+		}
+
+		exit(errcode);
+	}
+	cl_kernel kernel;
+	kernel = clCreateKernel(clProgram, "parent_vecadd", &errcode);
+	if (errcode != 0) {
+		printf("Error: %d\n", errcode);
+		exit(errcode);
 	}
 
-	cl_kernel clKernel;
-	clKernel = clCreateKernel(clProgram, "foo", &status);
-	if (status != 0) {
-		printf("Error when selecting the kernel to execute: %d\n", status);
-		exit(status);
-	}
 
-	
+	//-------------------------------------------------------//
+	//-------------Set the arguments of the kernel ----------//
+	status = clSetKernelArg(kernel, 0, sizeof(cl_mem), &bufferA);
+	status |= clSetKernelArg(kernel, 1, sizeof(cl_mem), &bufferB);
+	status |= clSetKernelArg(kernel, 2, sizeof(cl_mem), &bufferC);
+
+	//-------------------------------------------------------//
+	//-------------configure the work-item structure---------//
 	size_t globalWorkSize[1];
-	size_t localWorkSize[1];
-	globalWorkSize[0] = 8;
-	localWorkSize[0] = 8;
+	globalWorkSize[0] = elements;
 
-	size_t size = 8 * sizeof(float);
-
-	float* p =(float *) clSVMAlloc(
-
-		context,            // an OpenCL context where this buffer is available
-
-		CL_MEM_READ_ONLY,   // access mode for the kernel and other options; here
-		// only read-only access is required
-
-		size,               // amount of memory to allocate (in bytes)
-
-		0                   // alignment in bytes (0 means default)
-		);
-
-	if (p == NULL) {
-		printf("Error when allocating SVM memory");
-		exit(-1);
-	}
-
-
-	for (unsigned int i = 0; i < 8; i++) {
-		p[i] = 1.0 * i;
-	}
-
-
-	status = clSetKernelArgSVMPointer(clKernel, 0, p);
+	//-------------------------------------------------------//
+	//---------- ENQUEUE THE KERNEL EXECUTION ---------------//
+	status = clEnqueueNDRangeKernel(cmdQueue, kernel, 1, NULL, globalWorkSize, NULL, 0, NULL, NULL);
 	if (status != 0) {
-		printf("Error %d when setting a SVM buffer as argument of a kernel\n",status);
+		printf("Error about the kernel: %d\n", status);
+		while (true) {
+			;
+		}
 		exit(status);
 	}
 
-	status = clEnqueueNDRangeKernel(cmdQueue, clKernel, 1, 0, globalWorkSize, localWorkSize, 0, NULL, NULL);
+	//-------------------------------------------------------//
+	//----------- Read the output buffer back to the host ---//
+	cl_event event;
+	clEnqueueReadBuffer(cmdQueue, bufferC, CL_FALSE, 0, datasize, C, 0, NULL, &event);
+
+
+
+	status = clWaitForEvents(1, &event);
+	//status = clWaitForEvents(1, &readDone);
 	if (status != 0) {
-		printf("Errror %d when enqueuing the kernel for execution",status);
+		printf("Error: %d\n", status);
 		exit(status);
 	}
-	
-	//free host resources
-	clReleaseContext(context);
-	clReleaseCommandQueue(cmdQueue);
+	//print the result
+	for (int i = 0; i < elements; i++) {
+		printf("%d + %d = %d\n", A[i], B[i], C[i]);
+	}
+
+	clReleaseKernel(kernel);
 	clReleaseProgram(clProgram);
-	clReleaseKernel(clKernel);
-	free(devices);
+	clReleaseCommandQueue(cmdQueue);
+	clReleaseMemObject(bufferA);
+	clReleaseMemObject(bufferB);
+	clReleaseMemObject(bufferC);
+	clReleaseContext(context);
+	clReleaseEvent(event);
+	free(A);
+	free(B);
+	free(C);
 	free(platforms);
-	free(programStr);
+	free(devices);
+
 	return 0;
 }
